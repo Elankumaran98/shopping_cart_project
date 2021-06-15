7cd900dad9587cce146c1b2121a30389fe676c8d
Elankumaran98 <siva98kumarane@gmail.com>
2021-06-15 12:05:25 +0530

    bug fixed

--- lib/pages/welcomepage.dart
+++ lib/pages/welcomepage.dart
@@ -7,40 +7,52 @@ class WelcomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: Colors.black,
-        child: Stack(
-          children: [
-            Positioned.fill(
-                child: Opacity(
-              opacity: 0.3,
-              child: Image.asset(
-                '/assets/imgs/of_main_bg.png',
-                fit: BoxFit.cover,
-              ),
-            )),
-            Center(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Center(
-                    child: ClipOval(
-                      child: Container(
-                        width: 180,
-                        height: 180,
-                        color: AppColors.MAIN_COLOR,
-                        alignment: Alignment.center,
-                        child: IconFont(iconName: IconFontHelper.MAIN_LOGO),
+        body: Container(
+            color: Colors.black,
+            child: Stack(
+              children: [
+                Positioned.fill(
+                  child: Opacity(
+                    opacity: 0.3,
+                    child: Image.asset('assets/imgs/of_main_bg.png',
+                        fit: BoxFit.cover),
+                  ),
+                ),
+                Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Center(
+                        child: ClipOval(
+                          child: Container(
+                            width: 180,
+                            height: 180,
+                            color: AppColors.MAIN_COLOR,
+                            alignment: Alignment.center,
+                            child: IconFont(
+                                iconName: IconFontHelper.MAIN_LOGO,
+                                color: Colors.white,
+                                size: 130),
+                          ),
+                        ),
                       ),
-                    ),
-                  )
-                ],
-              ),
-            )
-          ],
-        ),
-      ),
-    );
+                      SizedBox(height: 50),
+                      Text('Welcome',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 40,
+                              fontWeight: FontWeight.bold)),
+                      SizedBox(height: 40),
+                      Text('Fresh Products.\nHealthy On time ',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(color: Colors.white, fontSize: 18)),
+                      SizedBox(height: 40),
+                    ],
+                  ),
+                )
+              ],
+            )));
   }
 }
